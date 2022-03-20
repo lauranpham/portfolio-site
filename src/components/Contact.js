68f357f113d35bc8b506afee2f1d9a178b8ee90d
Laura Pham <l.pham2311@gmail.com>
2022-03-20 17:36:39 +1100

    personal website: updated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,49 +1,93 @@
-import {useState} from 'react';
+import { useState } from 'react';
+const FORM_ENDPOINT =
+	'https://public.herotofu.com/v1/7299ee40-a80b-11ec-975b-f9cc2b0e6900';
 
 const Contact = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-  const [error, setError] = useState({name: false, email: false, message: false})
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
+	const [submitted, setSubmitted] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    // todo: handle form submit
-  }
-  return (
-    <section className='section'>
-      <div className='title'>
+	const handleSubmit = () => {
+		setTimeout(() => {
+			setSubmitted(true);
+		}, 100);
+	};
+
+	if (submitted) {
+		return (
+			<section className='section'>
+				<div className='title'>
+					<h2>Contact</h2>
+					<div className='underline'></div>
+                    <p>Thank you!</p>
+                    <p>We'll be in touch soon</p>
+				</div>
+			</section>
+		);
+	}
+
+	return (
+		<section className='section'>
+			<div className='title'>
 				<h2>Contact</h2>
 				<div className='underline'></div>
 			</div>
-      <form onSubmit={handleSubmit} className="contact-form">
-        <input
-            type='text'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder='John Smith'
-            className={`${error.name ? 'error' : null}`}
-          />
-          <input
-            type='text'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder='123@email.com'
-            className={`${error.email ? 'error' : null}`}
-          />
-          <input
-            type='text'
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder='your message'
-            className={`${error.message ? 'error' : null}`}
-          />
-          <button className='btn' type='submit'>
-            submit
-          </button>
-        </form>
-    </section>
-  );
+			{/* <form
+				action={FORM_ENDPOINT}
+				method="post"
+				onSubmit={handleSubmit}
+				className='contact-form'
+                target="_blank"
+			>
+				<input
+					type='text'
+					value={name}
+                    id="name"
+					onChange={(e) => setName(e.target.value)}
+					placeholder='John Smith'
+                    required
+				/>
+				<input
+					type='email'
+					value={email}
+                    id="email"
+					onChange={(e) => setEmail(e.target.value)}
+					placeholder='123@email.com'
+                    required
+				/>
+				<input
+					type='text'
+					value={message}
+                    id="message"
+					onChange={(e) => setMessage(e.target.value)}
+					placeholder='your message'
+                    required
+				/>
+				<button className='btn' type='submit'>
+					submit
+				</button>
+			</form> */}
+
+            <form action={FORM_ENDPOINT} method="post" className='contact-form'>
+  {/* <div>
+    <label for="name">Your Name</label> */}
+    <input name="Name" id="name" type="text" required placeholder="Name"/>
+  {/* </div>
+  <div>
+    <label for="email">Your Email</label> */}
+    <input name="Email" id="email" type="email" required  placeholder="Email"/>
+  {/* </div>
+  <div>
+    <label for="message">Your Message</label> */}
+    <input name="Message" id="messge" type="text" required placeholder="Message" />
+  {/* </div>
+  <div>  */}
+    <input type="submit" value="Submit" className="submit-btn" />
+  {/* </div> */}
+</form>
+		</section>
+	);
 };
 
 export default Contact;
